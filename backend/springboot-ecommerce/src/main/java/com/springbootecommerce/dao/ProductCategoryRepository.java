45095fae55577fb7009e8bfa0bb373e79fd8fc98
abelhajsalah <abelhajsalah@deloitte.tn>
2024-01-03 14:55:25 +0100

    add first entities

---
+++ backend/springboot-ecommerce/src/main/java/com/springbootecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.springbootecommerce.dao;
+
+import com.springbootecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<Product, Long> {
+}
