8bc776746a73a82712e9ce972efd9c3d52621e92
Ahmed Belhaj Salah <ahmedbhs39@gmail.com>
2024-01-24 10:52:43 +0100

    add review card behaviour

---
+++ frontend/angular-ecommerce/src/app/validators/shop-validators.ts
@@ -0,0 +1,17 @@
+import { FormControl, ValidationErrors } from "@angular/forms";
+
+export class ShopValidators {
+
+    // whitespace validation
+    static notOnlyWhiteSpace(control: FormControl): ValidationErrors | null{
+        //check if string only contains whitespaces
+        if((control.value != null) && (control.value.trim().length === 0)){
+            //invalid, return error object
+            return {'notOnlyWhiteSpace':true};
+        }
+        else{
+            //valid, return null
+            return null;
+        }
+    }
+}
